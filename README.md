30670300e4add13440fda3bccf7160cdc0ef810c
Chizzy Ibemeru <ibemeruc@accessbankplc.com>
2023-12-10 19:50:56 +0100

    scoremaster first push

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# ScoreMaster
+# ScoreMaster Console Application
+The ScoreMaster Console Application calculates a total score based on user-provided input and configurable scoring rules. This README provides instructions on how to use the application and what is expected from the user.
+
+## Table of Contents
+- Getting Started
+- Prerequisites
+- Installation
+- Usage
+- Configuration
+- Contributing
+
+## Getting Started
+### Prerequisites
+Before using the ScoreMaster Console Application, ensure you have the following prerequisites installed:
+
+- .NET SDK (compatible with .NET 7)
+
+## Installation
+Clone or download this repository to your local machine.
+
+Open a command prompt or terminal in the project directory.
+
+Build the application by running the following command:
+
+    ```bash    
+    dotnet build
+
+Run the application with the following command:
+
+	```bash    
+    dotnet run
+
+## Usage
+Launch the ScoreMaster Console Application by following the installation steps above.
+
+The application will prompt you to enter an array of integers separated by spaces. For example, you can enter:
+
+	```mathematica    
+	Enter an array of integers separated by spaces:
+	1 2 3 4 5
+
+Press Enter after entering the values.
+
+The application will calculate the total score based on the provided array and the configured scoring rules and display the result. For example:
+
+	```mathematica    
+    Total Score: 11
+
+You can run the application multiple times with different input arrays to calculate scores.
+
+Configuration
+The ScoreMaster Console Application allows you to configure scoring rules in the appsettings.json file. By default, the file contains the following configuration:
+
+	```json
+	{
+	  "ScoringRules": {
+		"EvenPoints": 1,
+		"OddPoints": 3,
+		"EightPoints": 5
+	  }
+	}
+You can adjust the values of EvenPoints, OddPoints, and EightPoints to change the scoring rules. The application will read these values from the configuration file.
+
+Contributing
+Contributions to this project are welcome. To contribute, follow these steps:
+
+## Fork the repository.
+
+Create a new branch for your changes.
+
+Make your changes and test them.
+
+Submit a pull request with a clear description of your changes.
---
+++ ScoreMaster/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var config = LoadConfiguration();
+
+            int[] inputArray = GetUserInputArray();
+            int evenPoints = GetConfigValue(config, "ScoringRules:EvenPoints", 1);
+            int oddPoints = GetConfigValue(config, "ScoringRules:OddPoints", 3);
+            int eightPoints = GetConfigValue(config, "ScoringRules:EightPoints", 5);
+
+            int score = CalculateTotalScore(inputArray, evenPoints, oddPoints, eightPoints);
+
+            Console.WriteLine("Total Score: " + score);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+    static IConfiguration LoadConfiguration()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        return config;
+    }
+
+    static int[] GetUserInputArray()
+    {
+        Console.WriteLine("Enter an array of integers separated by spaces:");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid input. Please enter integers separated by spaces.");
+                continue;
+            }
+
+            string[] inputValues = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int[] inputArray = new int[inputValues.Length];
+
+            for (int i = 0; i < inputValues.Length; i++)
+            {
+                if (int.TryParse(inputValues[i], out int value))
+                {
+                    inputArray[i] = value;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid input: '{inputValues[i]}'. Please enter valid integers.");
+                    return GetUserInputArray();
+                }
+            }
+
+            return inputArray;
+        }
+    }
+
+    static int CalculateTotalScore(int[] arr, int evenPoints, int oddPoints, int eightPoints)
+    {
+        int score = 0;
+
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                score += evenPoints; // Add points for even numbers
+            }
+            else
+            {
+                score += oddPoints; // Add points for odd numbers
+            }
+
+            if (num == 8)
+            {
+                score += eightPoints; // Add points for each occurrence of 8
+            }
+        }
+
+        return score;
+    }
+
+    static int GetConfigValue(IConfiguration config, string key, int defaultValue)
+    {
+        string value = config[key];
+        if (int.TryParse(value, out int intValue))
+        {
+            return intValue;
+        }
+
+        return defaultValue;
+    }
+}
